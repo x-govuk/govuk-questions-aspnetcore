b4bba5a5ae2c6d67c5504e0f0281c474a2a4b064
James Gunn <james@gunn.io>
2026-01-01 15:40:26 +0000

    Use returnUrl query param if specified when redirecting to next page

--- src/GovUk.Questions.Mvc/JourneyCoordinator.cs
+++ src/GovUk.Questions.Mvc/JourneyCoordinator.cs
@@ -12,6 +12,11 @@ namespace GovUk.Questions.Mvc;
 /// </summary>
 public abstract class JourneyCoordinator
 {
+    /// <summary>
+    /// The name of the query parameter used to specify the return URL.
+    /// </summary>
+    public const string ReturnUrlQueryParameterName = "returnUrl";
+
     private CoordinatorContext? _context;
 
     private bool _deleted;
@@ -293,6 +298,68 @@ public abstract class JourneyCoordinator
         return UpdateStateStorageEntryCoreAsync(async e => e with { State = await getNewState(e.State) }).AsTask();
     }
 
+    private static bool IsLocalUrl(string url)
+    {
+        // https://source.dot.net/#Microsoft.AspNetCore.Http.Results/src/Shared/ResultsHelpers/SharedUrlHelper.cs
+
+        if (string.IsNullOrEmpty(url))
+        {
+            return false;
+        }
+
+        // Allows "/" or "/foo" but not "//" or "/\".
+        if (url[0] == '/')
+        {
+            // url is exactly "/"
+            if (url.Length == 1)
+            {
+                return true;
+            }
+
+            // url doesn't start with "//" or "/\"
+            if (url[1] != '/' && url[1] != '\\')
+            {
+                return !HasControlCharacter(url.AsSpan(1));
+            }
+
+            return false;
+        }
+
+        // Allows "~/" or "~/foo" but not "~//" or "~/\".
+        if (url[0] == '~' && url.Length > 1 && url[1] == '/')
+        {
+            // url is exactly "~/"
+            if (url.Length == 2)
+            {
+                return true;
+            }
+
+            // url doesn't start with "~//" or "~/\"
+            if (url[2] != '/' && url[2] != '\\')
+            {
+                return !HasControlCharacter(url.AsSpan(2));
+            }
+
+            return false;
+        }
+
+        return false;
+
+        static bool HasControlCharacter(ReadOnlySpan<char> readOnlySpan)
+        {
+            // URLs may not contain ASCII control characters.
+            for (var i = 0; i < readOnlySpan.Length; i++)
+            {
+                if (char.IsControl(readOnlySpan[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
     private async ValueTask<IActionResult> AdvanceCoreAsync(
         string nextStepUrl,
         Func<object, ValueTask<object>> getNewState,
@@ -311,6 +378,13 @@ public abstract class JourneyCoordinator
 
             return new StateStorageEntry { State = newState, Path = newPath };
         });
+
+        // Check if there's an explicit return URL provided
+        if (HttpContext.Request.Query.TryGetValue(ReturnUrlQueryParameterName, out var returnUrlValues) &&
+            returnUrlValues.ToString() is string returnUrl && IsLocalUrl(returnUrl))
+        {
+            return new RedirectResult(returnUrl);
+        }
 
         return new RedirectResult(nextStepUrl);
     }
